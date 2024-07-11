1c280edf9a3b2273efd1ef44bb05f45ae029d03f
yuvraj singh shori <yuvrajshori5@gmail.com>
2024-07-11 22:18:03 +0530

    completed all routes and add them into app.js

--- src/app.js
+++ src/app.js
@@ -18,8 +18,24 @@ app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.js";
+import healthcheckRouter from "./routes/healthcheck.js";
+import tweetRouter from "./routes/tweet.js";
+import subscriptionRouter from "./routes/subscription.js";
+import videoRouter from "./routes/video.js";
+import commentRouter from "./routes/comment.js";
+import likeRouter from "./routes/like.js";
+import playlistRouter from "./routes/playlist.js";
+import dashboardRouter from "./routes/dashboard.js";
 
 //routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
